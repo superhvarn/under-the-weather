daec5a83f20787b2a6b411e8d66095bd561c5654
alanz <zhang.alan@northeastern.edu>
2024-02-24 08:25:12 -0500

    Added handleSubmit for ZipCode

--- src/Authentification/ZipCode.tsx
+++ src/Authentification/ZipCode.tsx
@@ -3,10 +3,16 @@ import {useState} from "react";
 export const ZipCode = (props: any) => {
     const [zipCode, setZipCode] = useState('');
 
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        console.log("Submit button pressed")
+    }
+
     return (
         <div className={"auth-form-container"}>
             <h1>Enter a Zip Code to get Started!</h1>
             <input type={"text"} placeholder={"01234"} value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
+            <button type={"submit"} onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
