22fb199e3bfe45edf43314256c6451d7c2959212
harish <harivarada1@gmail.com>
2024-02-24 20:23:13 -0500

    fixed issue with reloading going back to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import React, {useState} from 'react';
-import {Register} from "./Authentification/Register"
-import {Login} from "./Authentification/Login"
-import {ZipCode} from "./Authentification/ZipCode";
+import React, { useState, useEffect } from 'react';
+import { Register } from "./Authentification/Register"
+import { Login } from "./Authentification/Login"
+import { ZipCode } from "./Authentification/ZipCode";
 import './Authentification/authentication.css'
-import {HomePage} from "./HomePage/HomePage";
+import { HomePage } from "./HomePage/HomePage";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-  const togglePage = (pageName: string) => {
-    setCurrentPage(pageName)
+  // Initialize currentPage from localStorage if available, otherwise default to 'home'
+  const [currentPage, setCurrentPage] = useState(localStorage.getItem('currentPage') || 'home');
+
+  useEffect(() => {
+    // Update localStorage whenever currentPage changes
+    localStorage.setItem('currentPage', currentPage);
+  }, [currentPage]);
+
+  const togglePage = (pageName : any) => {
+    setCurrentPage(pageName);
   }
 
   return (
-      <div className="App">
-          <header className="App-header">
-              {currentPage === "login" ? (
-                  <Login onFormSwitch={togglePage}/>
-              ) : currentPage === "zipcode" ? (
-                  <ZipCode onFormSwitch={togglePage}/>
-              ) : currentPage === "register" ? (
-                  <Register onFormSwitch={togglePage}/>
-              ) : (
-                  <HomePage onFormSwitch={togglePage}/>
-              )
-              }
-          </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        {currentPage === "login" ? (
+          <Login onFormSwitch={togglePage} />
+        ) : currentPage === "zipcode" ? (
+          <ZipCode onFormSwitch={togglePage} />
+        ) : currentPage === "register" ? (
+          <Register onFormSwitch={togglePage} />
+        ) : (
+          <HomePage onFormSwitch={togglePage} />
+        )}
+      </header>
+    </div>
   );
 }
 
