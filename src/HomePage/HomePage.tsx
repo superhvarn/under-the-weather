845addab51b2ed3c6bd9251b97df1f55c14c3033
alanZhang0813 <132928424+alanZhang0813@users.noreply.github.com>
2024-02-24 22:22:19 -0500

    Added icons, new favicon, etc

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import './HomePage.css';
+import { FaUserCheck, FaUserPlus, FaQuestionCircle } from "react-icons/fa";
+import { FaPeopleGroup } from "react-icons/fa6";
 
 const HomePage = (props: any) => {
     return (
         <div className="home-page">
+            <div className={""}></div>
+
             <img src={`${process.env.PUBLIC_URL}/logo.png`} width={250} height={250} alt="Logo" className="homepage-logo"/>
             <video autoPlay={true} loop={true} muted={true} className={"background-video"}>
                 <source src={"/Videos/rain_in_reverse.mp4"} type={'video/mp4'}/>
             </video>
-
             <span className="text-gradient">Under the Weather</span>
             <div className="button-container">
-                <button onClick={() => props.onFormSwitch("login")}>Login</button>
-                <button onClick={() => props.onFormSwitch("register")}>Register</button>
+                <button className={"custom-button"} onClick={() => props.onFormSwitch("login")}>
+                    <FaUserCheck/>
+                    <br/>
+                    Login
+                </button>
+                <button className={"custom-button"} onClick={() => props.onFormSwitch()}>
+                    <FaQuestionCircle/>
+                    <br/>
+                    Help
+                </button>
+                <button className={"custom-button"} onClick={() => props.onFormSwitch()}>
+                    <FaPeopleGroup/>
+                    <br/>
+                    About Us
+                </button>
+                <button className={"custom-button"} onClick={() => props.onFormSwitch("register")}>
+                    <FaUserPlus/>
+                    <br/>
+                    Register
+                </button>
             </div>
         </div>
     );
