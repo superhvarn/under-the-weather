bfd72661da46a9ef37b0a3b311454c5f598a94ed
EthanMoskowitz <ethanmoskowitz0@gmail.com>
2024-02-25 15:10:11 -0500

    Require Register

--- src/Authentication/Register.tsx
+++ src/Authentication/Register.tsx
@@ -34,19 +34,19 @@ export const Register = (props: any) => {
             <form className={"register-form"} onSubmit={handleSubmit}>
                 <label htmlFor={"First Name"}>First Name</label>
                 <input value={firstName} onChange={(e) => setFirstName(e.target.value)}
-                       type={"text"} placeholder={"First Name"} id={"firstName"} name={"firstName"}/>
+                       type={"text"} placeholder={"First Name"} id={"firstName"} name={"firstName"} required/>
 
                 <label htmlFor={"Last Name"}>Last Name</label>
                 <input value={lastName} onChange={(e) => setLastName(e.target.value)}
-                       type={"text"} placeholder={"Last Name"} id={"lastName"} name={"lastName"}/>
+                       type={"text"} placeholder={"Last Name"} id={"lastName"} name={"lastName"} required/>
 
                 <label htmlFor={"email"}>Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)}
-                       type={"email"} placeholder={"your-email@gmail.com"} id={"email"} name={"email"}/>
+                       type={"email"} placeholder={"your-email@gmail.com"} id={"email"} name={"email"} required/>
 
                 <label htmlFor={"password"}>Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)}
-                       type={"password"} placeholder={"your-password!@#"} id={"password"} name={"password"}/>
+                       type={"password"} placeholder={"your-password!@#"} id={"password"} name={"password"} required/>
 
                 <button type={"submit"}>Create Account</button>
             </form>
